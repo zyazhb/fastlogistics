86806212d690ccbc8a4e690766fcb443b6865905
zyazhb <zyazhb@yahoo.com>
2020-10-28 09:27:20 +0800

    Try static map

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+mapkey = "510032adf681277699fd944dce14365f"
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/map")
+def getmap(markers="mid,0xFF0000,A:116.37359,39.92437;116.47359,39.92437"):
+    return "https://restapi.amap.com/v3/staticmap?markers="+markers+"&key="+mapkey
 
 
 @app.get("/items/{item_id}")
