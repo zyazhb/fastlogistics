83da0a591ecc1c1250f00d7d3782ac39258e0012
root <2609957581@qq.com>
2021-01-06 10:24:50 +0800

    Fix template bug

--- waypoints.py
+++ waypoints.py
@@ -36,19 +36,13 @@ def waypoints():
 @app.route('/move')
 def move():
     plist = []
-    try:
-        jsondata = requests.get(
-            "https://ae770067-27eb-4c02-b5cd-9620231fb68e.bspapp.com/http/get", timeout=3)
-        jsondata = jsondata.replace("\'", "\"")
-        # print(jsondata)
-        data = json.loads(jsondata)
-        for point in data['data'][0]['markers']:
-            plist.append([float(point['longitude']), float(point['latitude'])])
-        print(plist)
-    except:
-        plist = [[116.478935, 39.997761], [116.478939, 39.997825], [116.478912, 39.998549], [116.478912, 39.998549], [116.478998, 39.998555], [116.478998, 39.998555], [116.479282, 39.99856], [116.479658, 39.998528], [116.480151, 39.998453], [
-            116.480784, 39.998302], [116.480784, 39.998302], [116.481149, 39.998184], [116.481573, 39.997997], [116.481863, 39.997846], [116.482072, 39.997718], [116.482362, 39.997718], [116.483633, 39.998935], [116.48367, 39.998968], [116.484648, 39.999861]]
-
+    jsondata = requests.get("https://ae770067-27eb-4c02-b5cd-9620231fb68e.bspapp.com/http/get")
+    jsondata = jsondata.text.replace("\'", "\"")
+    print(jsondata)
+    data = json.loads(jsondata)
+    for point in data['data'][0]['markers']:
+        plist.append([float(point['longitude']), float(point['latitude'])])
+    print(plist)
     return render_template('move.html', plist=plist)
 
 
